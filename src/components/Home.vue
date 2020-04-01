f964136d7a7fcf66698265ec5eba4c55283fa35f
Juan Fretes <fretesjuan4@gmail.com>
2020-03-31 20:01:11 -0400

    cambio a vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <NavigationBar/>
+  </v-container>
+</template>
+
+<script>
+import NavigationBar from "./NavigationBar.vue";
+export default {
+  name: "Home",
+   components: {
+    NavigationBar,
+  },
+  data: () => ({})
+};
+</script>
