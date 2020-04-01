c196e03144037e49bc476bc7aa299ad08e757bca
Juan Fretes <fretesjuan4@gmail.com>
2020-03-31 20:48:18 -0400

    add cositas

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,68 +1,58 @@
 <template>
+  <v-app-bar
+    absolute
+    color="#6A76AB"
+    dark
+    shrink-on-scroll
+    prominent
+    src="https://images.unsplash.com/photo-1525362081669-2b476bb628c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+    fade-img-on-scroll
+    scroll-target="#scrolling-techniques-4"
+  >
+    <template v-slot:img="{ props }">
+      <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
+    </template>
 
-    <v-app-bar
-      absolute
-      color="#6A76AB"
-      dark
-      shrink-on-scroll
-      prominent
-      src="https://picsum.photos/1920/1080?random"
-      fade-img-on-scroll
-      scroll-target="#scrolling-techniques-4"
-    >
-      <template v-slot:img="{ props }">
-        <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
-      </template>
+    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+    <v-spacer></v-spacer>
+    
+    <v-toolbar-title class="page-title">JDA beats</v-toolbar-title>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
 
-      <v-toolbar-title>JDA beats</v-toolbar-title>
+    <v-btn icon>
+      <v-icon>mdi-instagram</v-icon>
+    </v-btn>
 
-      <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-soundcloud</v-icon>
+    </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-youtube</v-icon>
+    </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-menu bottom left>
-        <template v-slot:activator="{ on }">
-          <v-btn icon color="yellow" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="(item, i) in items" :key="i">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-
-      <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Home</v-tab>
-          <v-tab>Beats</v-tab>
-          <v-tab>Contact</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-
+    <template v-slot:extension>
+      <v-tabs align-with-title>
+        <v-tab>Home</v-tab>
+        <v-tab>Beats</v-tab>
+        <!-- <v-tab>Contact</v-tab> -->
+      </v-tabs>
+    </template>
+  </v-app-bar>
 </template>
 <script>
 export default {
   name: "NavigationBar",
 
   data: () => ({
-    items: [
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me 2" }
-    ]
+
   })
 };
 </script>
+
+<style>
+.page-title {
+    font-size: 2.5rem !important;
+}
+</style>
