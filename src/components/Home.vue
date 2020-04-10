b2bd2be8282d186dad27d222de457ae50ee9325a
Juan Fretes <fretesjuan4@gmail.com>
2020-04-09 20:24:49 -0400

    delte spotify frames

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,49 +11,10 @@
             height="140"
             style="border: none;"
           ></iframe>
-  
-          <iframe
-            src="https://open.spotify.com/embed/track/5hVpPjIWHrS3icQIjMOb3v"
-            width="90%"
-            height="380"
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
-          ></iframe>
-
-          <iframe
-            src="https://open.spotify.com/embed/track/4Z0SznvLhlEQ5qRHxqFe8d"
-            width="90%"
-            height="380"
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
-          ></iframe>
-
-          <iframe
-            src="https://open.spotify.com/embed/track/5SCztdGLUnkYMB47qEyDNA"
-            width="90%"
-            height="380"
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
-          ></iframe>
-
-          <iframe
-            src="https://open.spotify.com/embed/track/2VCU5KT8IC4EyYFati0W8U"
-            width="90%"
-            height="380"
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media"
-          ></iframe>
+      
         </v-content>
       </v-container>
     </v-content>
-
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2019 JDA</span>
-    </v-footer>
   </div>
 </template>
 
